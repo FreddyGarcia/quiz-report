a317602a94c2f810ae35f23343fdf76e607ebb89
Freddy Garcia Abreu <buscando_alink@hotmail.com>
2020-08-03 13:44:47 -0400

    Build api

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, Response
 from pymongo import MongoClient
 from bson.json_util import dumps
+from pandas import DataFrame
 
 app = Flask(__name__)
 client = MongoClient("mongodb+srv://readonly:6LejsYJEHZDe5qZF@cluster0-6iwz3.mongodb.net/british-quiz-bot?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
@@ -8,30 +9,6 @@ client = MongoClient("mongodb+srv://readonly:6LejsYJEHZDe5qZF@cluster0-6iwz3.mon
 db = client['british-quiz-bot']
 questions = db.questions
 tests = db.tests
-users = db.users
-
-
-def count_answer(tests, question_id):
-
-    c = {
-        'A' : 0,
-        'B' : 0,
-        'C' : 0,
-        'D' : 0,
-    }
-
-    for test in tests:
-        if test['question'] == question_id:
-            if 'A' in test['answer']:
-                c['A'] += 1
-            elif 'B' in test['answer']:
-                c['B'] += 1
-            elif 'C' in test['answer']:
-                c['C'] += 1
-            elif 'D' in test['answer']:
-                c['D'] += 1
-            
-    return c
 
 
 def get_tests_data():
@@ -40,9 +17,7 @@ def get_tests_data():
         'questions.question': True,
         'questions.answeredCorrectly': True
     })
-    # .limit(10)
 
-    # rows = list(map( lambda x: x['questions'] , query ))
     rows = list(map( lambda x: dict(test_id=x['_id'], questions=x['questions']) , query ))
     questions = []
     
@@ -57,30 +32,83 @@ def get_tests_data():
 def get_questions_data():
     query = questions.find({}, {
         'fields.question_text': True,
+        'fields.airtable_id': True,
         'fields.A': True,
         'fields.B': True,
         'fields.C': True,
         'fields.D': True
     })
-    # .limit(10)
 
     rows = list(map( lambda x: dict(id=x['_id'], **x['fields']) , query ))
     return rows
 
 
+def count_answers(tests, question_id):
+
+    c = {
+        'ChoseA' : 0,
+        'ChoseB' : 0,
+        'ChoseC' : 0,
+        'ChoseD' : 0,
+    }
+
+    for test in tests:
+        if test['question'] == question_id:
+            if 'A' in test['answer']:
+                c['ChoseA'] += 1
+            elif 'B' in test['answer']:
+                c['ChoseB'] += 1
+            elif 'C' in test['answer']:
+                c['ChoseC'] += 1
+            elif 'D' in test['answer']:
+                c['ChoseD'] += 1
+            
+    return c
+
+
 def prepare_data(questions, tests):
 
     for question in questions:
-        counts = count_answer(tests, question['id'])
+        counts = count_answers(tests, question['id'])
         question.update(counts)
+
+    df_tests = DataFrame(tests)
+    df_questions = DataFrame(questions)
+
+    df_questions.set_index('id', inplace=True)
+    df_tests.answer = df_tests.answer.map(lambda x: ','.join(x))   
+    df_tests.drop_duplicates(subset=['test_id', 'question'], inplace=True)
+
+    cols = ['question', 'test_id', 'answeredCorrectly']
+    s_correct = df_tests[df_tests.answeredCorrectly == 'true'].groupby(cols).count().groupby('question').sum()
+    s_wrong = df_tests[df_tests.answeredCorrectly == 'false'].groupby(cols).count().groupby('question').sum()
+    df_questions = df_questions.join(s_correct)
+    df_questions.rename(columns={'answer' : 'Correct'}, inplace=True)
+    df_questions = df_questions.join(s_wrong)
+    df_questions.rename(columns={'answer' : 'Wrong'}, inplace=True)
+
+    df_questions['Attemps'] = df_questions['Correct'] + df_questions['Wrong']
+    df_questions['% Correct'] = df_questions['Correct'] / df_questions['Attemps']
 
     return questions
 
 
-@app.route('/')
-def index():
+@app.route('/questions')
+def questions():
+    # retrieve records from db
     questions = get_questions_data()
     tests = get_tests_data()
-    # rows = prepare_data(questions, tests)
 
-    return jsonify(rows)
+    # perform calculations
+    df_questions = prepare_data(questions, tests)
+    json = df_questions.to_json(orient='records')
+
+    # define response
+    res = Response(json)
+    res.headers['Content-Type'] = 'application/json'
+
+    return res
+
+@app.route('/')
+def index():
+    return render_template('index.html')
